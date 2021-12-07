6a5acc906e01fb9b21fc22658f9c18372647665c
Opeyemi Bankole <opeyemi33171@gmail.com>
2021-12-07 22:40:29 +0000

    adds ability to short urls

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
-import express from "express";
+import express, { urlencoded } from "express";
+import randomString from "randomstring";
 
 let shortenedUrls = [];
 
-
 const app = express();
 
+app.use(express.static("static"));
+app.use(express.urlencoded({extended: true}));
+
 app.get("/", (req, res) => {
-    res.send("Welcome Opey!");
+    res.render("index.html")
+});
+
+app.post("/", (req, res) => {
+    let short = ""
+    if(shortenedUrls[req.body.url] == undefined){
+        short = randomString.generate(5);
+
+        shortenedUrls.push({
+            [req.body.url]: short
+        });
+    }
+    console.log(shortenedUrls);
+    res.send(`localhost:2000/${short}`);
+});
+
+app.get("/:short", (req, res) => {
+    shortenedUrls.forEach((url) => {
+        if(Object.values(url).includes(req.params.short)){
+            res.redirect(Object.keys(url)[0]);
+        }
+    });
+
+    res.send("short doesn't exist");
 });
 
 app.listen(2000, () => {
